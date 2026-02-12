eb8855841c514bf9fff559177bd1971e6b968fe8
akdwivedi-explorer <akdwivediofficial@outlook.com>
2026-02-12 21:44:00 +0530

    CustomerNotFoundException fix

--- src/main/java/com/ashutosh/restaurant_management/exception/CustomerNotFoundException.java
+++ src/main/java/com/ashutosh/restaurant_management/exception/CustomerNotFoundException.java
@@ -1,7 +1,7 @@
 package com.ashutosh.restaurant_management.exception;
 
 public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(String message) {
-        super(message);
+    public CustomerNotFoundException() {
+        super("Customer not found exception");
     }
 }
