02a0c9fb39b36e539b85b9debc43d62c6d47327c
akdwivedi-explorer <akdwivediofficial@outlook.com>
2026-02-16 00:40:38 +0530

    Order Dto and Model minor fix

---
+++ src/main/java/com/ashutosh/restaurant_management/dto/OrderDetailDto.java
@@ -0,0 +1,21 @@
+package com.ashutosh.restaurant_management.dto;
+
+import com.ashutosh.restaurant_management.enums.OrderStatus;
+import com.ashutosh.restaurant_management.enums.OrderType;
+import com.ashutosh.restaurant_management.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDetailDto {
+    private int restaurantId;
+    private int customerId;
+    private OrderType orderType;
+    private OrderStatus orderStatus;
+    private double totalAmount;
+    private PaymentStatus paymentStatus;
+
+}
